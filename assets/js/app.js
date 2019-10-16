85ab9821ff72895ba9aae3cef8a95a74744cab41
阿部寛子 <hihoyeahi@gmail.com>
2019-10-16 17:45:26 +0900

    背景に奥行き／動きを追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 setInterval("goFlash()",9000);
 
 function goFlash() {
-    $('.keybox').addClass('holizonal_sec');
+    $('.keybox').addClass('vertical_sec');
     setTimeout("removeFlash()",1000);
 }
 
 function removeFlash() {
-    $('.keybox').removeClass('holizonal_sec');
+    $('.keybox').removeClass('vertical_sec');
 }
 
